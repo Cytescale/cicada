b87a85fbd6e9bc52e53f069fa0a86d0e093da0d6
cytescale <nikhil@cytescale.com>
2021-06-21 23:39:19 +0530

    server commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,23 @@ import { useState } from 'react'
 import FullHeiLoading from './src/fullHeightLoading';
 
 import LoginAct from './src/login'
+import LandAct from './src/land';
+
+const DeciderComp:React.FC<any> = (props:any)=>{
+  if(props.isAuth){
+    return <LandAct {...props}/>
+  }else{
+    return <LoginAct {...props}/> 
+  }
+}
 
 export default function Home() {
   let [isAuth , setAuth ] = useState<boolean>(false);
   let [loading , setLoading] = useState<boolean>(false);
-
-  return (
   
+  return (
       !loading?
-      <LoginAct 
+      <DeciderComp
       isAuth={isAuth} 
       setAuth={setAuth} 
       loading={loading}

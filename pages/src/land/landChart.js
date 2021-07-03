79c41375cd26389a710802a4e6341854cc75f9f9
cytescale <nikhil@cytescale.com>
2021-07-04 02:50:38 +0530

    server commit

--- pages/src/land/landChart.js
+++ pages/src/land/landChart.js
@@ -4,9 +4,9 @@ import { useEffect,useRef } from 'react';
    function renderVisitGraph(canvasRef){
           const canvas = canvasRef;
           let ctx  = canvas.current.getContext('2d');     
-          let gradient_2 = ctx.createLinearGradient(0, 0, 0,150);
+          let gradient_2 = ctx.createLinearGradient(0, 0, 0,190);
           gradient_2.addColorStop(1, 'rgba(255,255,255,0)');
-          gradient_2.addColorStop(0, '#7EFF9B');
+          gradient_2.addColorStop(0, '#55A3FF');
           let gradient = ctx.createLinearGradient(150, 0, 150,150);
           gradient.addColorStop(0, '#11E4D1');
           gradient.addColorStop(1, '#7EFF9B');
@@ -18,8 +18,8 @@ import { useEffect,useRef } from 'react';
                          fill:true,   
                         data:[100,100,200,400,250,500,200,270,350,210,240,160,70,103,120],
                         backgroundColor:gradient_2,
-                        borderColor: gradient,
-                        borderWidth: 5,
+                        borderColor: '#55A3FF',
+                        borderWidth: 4,
                         pointRadius: 0,
                         lineTension:0.5,
                         pointBorderWidth:0,
@@ -52,6 +52,7 @@ import { useEffect,useRef } from 'react';
                          }
                      },
                      x:{
+                         display:false,
                          grid:{
                               display:false
                          },

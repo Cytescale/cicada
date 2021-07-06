a5029c6ad5996c8c76ec6f0a0e0e66f3543fa6d1
cytescale <nikhil@cytescale.com>
2021-07-06 12:40:56 +0530

    server commit

--- pages/src/land/index.tsx
+++ pages/src/land/index.tsx
@@ -1231,7 +1231,7 @@ class Land extends React.Component<LandProps,any>{
                               <div className='app-land-lab-main-cont'>Cluster Link</div>
                               <div className='clust-link-main-cont'> 
                                    <div className='clust-link-main-lock-cont'>
-                                        <svg className='clust-link-main-lock-ico' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM9 8V6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9z"/></svg>
+                                        <svg className='clust-link-main-lock-ico' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM9 8V6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9z"/></svg>
                                    </div>
                                    <div className='clust-link-main-link'>
                                         https://cyte.com/t/{User?.getUserData()?.uname}

05e5c5b4abe1bb9bad189edfac6d2b8aa7e6e521
cytescale <nikhil@cytescale.com>
2021-07-07 22:10:23 +0530

    server commit

--- pages/src/land/globalStyle.js
+++ pages/src/land/globalStyle.js
@@ -2,20 +2,24 @@
 import { createGlobalStyle} from "styled-components"
 
 
- const GlobalStyles= createGlobalStyle`
-body {
-  --backColor:${({ theme }) => theme.backColor};
-  --borColor:${({ theme }) => theme.borColor};
-  --boxShad:${({ theme }) => theme.boxShad};
-  --linkHolderColor:${({ theme }) => theme.linkHolderColor};
-  --headerPriColor:${({ theme }) => theme.headerPriColor};
-  --headerSecColor:${({ theme }) => theme.headerSecColor};
-  --headColor:${({ theme }) => theme.headColor};
-  --buttonBackColor:${({ theme }) => theme.buttonBackColor};
-  --headBugerColor:${({ theme }) => theme.headBugerColor};
-  --cardColor:${({ theme }) => theme.cardColor};
-  --smallCardColor:${({ theme }) => theme.smallCardColor};
-}
+ const GlobalStyles= createGlobalStyle
+ `
+   
+
 `
 
 export default GlobalStyles;
+
+// body {
+//      --backColor:${({ theme }) => theme.backColor};
+//      --borColor:${({ theme }) => theme.borColor};
+//      --boxShad:${({ theme }) => theme.boxShad};
+//      --linkHolderColor:${({ theme }) => theme.linkHolderColor};
+//      --headerPriColor:${({ theme }) => theme.headerPriColor};
+//      --headerSecColor:${({ theme }) => theme.headerSecColor};
+//      --headColor:${({ theme }) => theme.headColor};
+//      --buttonBackColor:${({ theme }) => theme.buttonBackColor};
+//      --headBugerColor:${({ theme }) => theme.headBugerColor};
+//      --cardColor:${({ theme }) => theme.cardColor};
+//      --smallCardColor:${({ theme }) => theme.smallCardColor};
+//      }

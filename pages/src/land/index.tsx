67c531991d47dda0fa658d55bb34821ea9b298aa
cytescale <nikhil@cytescale.com>
2021-07-18 00:09:31 +0530

    server commit

--- pages/src/land/index.tsx
+++ pages/src/land/index.tsx
@@ -694,7 +694,7 @@ class Land extends React.Component<LandProps,any>{
                validated:false,
                isdeeplink:true,
                detailed:false,
-               
+               searchQuery:null,
           }
 
           this.initDataLoad = this.initDataLoad.bind(this);
@@ -722,8 +722,10 @@ class Land extends React.Component<LandProps,any>{
           this.setdetailed = this.setdetailed.bind(this);
           this.setlgoutConfirmVisi = this.setlgoutConfirmVisi.bind(this);
           this.setisdeeplink = this.setisdeeplink.bind(this);
+          this.setsearchQuery = this.setsearchQuery.bind(this);
      }
 
+     setsearchQuery(s:boolean){this.setState({searchQuery:s})}
      setisdeeplink(b:boolean){this.setState({isdeeplink:b})}
      setlgoutConfirmVisi(b:boolean){this.setState({lgoutConfirmVisi:b})}
      setdetailed(b:boolean){this.setState({detailed:b})}
@@ -1393,7 +1395,19 @@ class Land extends React.Component<LandProps,any>{
                               </button>
                               </div>
                               <div className='app-land-lab-main-cont'>Links {this.state.linksData.length}</div>
-                              
+                              <div className='app-land-serch-main-cont'>
+                                   <div className='app-land-serch-cont'>
+                                        <div className='app-land-serch-ico-cont'>
+                                             <svg className='app-land-serch-ico' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27c1.2-1.4 1.82-3.31 1.48-5.34-.47-2.78-2.79-5-5.59-5.34-4.23-.52-7.79 3.04-7.27 7.27.34 2.8 2.56 5.12 5.34 5.59 2.03.34 3.94-.28 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
+                                        </div>
+                                        <input
+                                        type='text'
+                                        className='app-land-serch-bar'
+                                        placeholder='Search for link'
+                                        />
+                                   </div>
+
+                              </div>
                               {
                               this.state.linkDataLoading?
                               <div className='app-land-rel-main-cont'>

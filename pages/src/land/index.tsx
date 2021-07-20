0d73316b94cd47a7c4d1014180ebcb180a171a84
cytescale <nikhil@cytescale.com>
2021-07-20 21:31:00 +0530

    server commit

--- pages/src/land/index.tsx
+++ pages/src/land/index.tsx
@@ -1287,7 +1287,9 @@ class Land extends React.Component<LandProps,any>{
                               
                               <div className='app-land-link-cont-holder top-holder-cont'>
                               <div className='app-land-link-intro-pro-main-cont'>
-                                        <div className='app-land-head-pro-pic-main-cont'></div>
+                                        <div className='app-land-head-pro-pic-main-cont'>
+                                             <svg  className='app-land-head-pro-pic-main-cont-pic' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="48px" height="48px"><radialGradient id="S2w73RYcJka7XmMmG50Ola" cx="23.801" cy="27.168" r="23.295" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ffcf54"/><stop offset=".261" stop-color="#fdcb4d"/><stop offset=".639" stop-color="#f7c13a"/><stop offset="1" stop-color="#f0b421"/></radialGradient><path fill="url(#S2w73RYcJka7XmMmG50Ola)" d="M44,23c0,2,0,7-3,7c-1.5,0-2.5-0.5-3-1v15H10V29c-0.5,0.5-1.5,1-3,1c-2.956,0-3-5-3-7	c0-8,7-18,20-18S44,15,44,23z"/><linearGradient id="S2w73RYcJka7XmMmG50Olb" x1="39.5" x2="39.5" y1="28.635" y2="44.054" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#c48f0c"/><stop offset="1" stop-color="#cd9713"/></linearGradient><path fill="url(#S2w73RYcJka7XmMmG50Olb)" d="M41,30v12c0,1.105-0.895,2-2,2h-1V29C38.5,29.5,39.5,30,41,30z"/><linearGradient id="S2w73RYcJka7XmMmG50Olc" x1="8.5" x2="8.5" y1="29.166" y2="44.279" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#c48f0c"/><stop offset="1" stop-color="#cd9713"/></linearGradient><path fill="url(#S2w73RYcJka7XmMmG50Olc)" d="M10,29v15H9c-1.105,0-2-0.895-2-2V30C8.5,30,9.5,29.5,10,29z"/><linearGradient id="S2w73RYcJka7XmMmG50Old" x1="23.08" x2="37.248" y1="7.513" y2="22.582" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#6d7479"/><stop offset="1" stop-color="#323538"/></linearGradient><circle cx="32" cy="17" r="5" fill="url(#S2w73RYcJka7XmMmG50Old)"/><linearGradient id="S2w73RYcJka7XmMmG50Ole" x1="32.072" x2="35.941" y1="11.611" y2="20.794" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fcfcfc"/><stop offset="1" stop-color="#c3c9cd"/></linearGradient><path fill="url(#S2w73RYcJka7XmMmG50Ole)" d="M32,12c-0.688,0-1.342,0.14-1.939,0.391C30.037,12.593,30,12.791,30,13c0,2.761,2.239,5,5,5	c0.688,0,1.342-0.14,1.939-0.391C36.963,17.407,37,17.209,37,17C37,14.239,34.761,12,32,12z"/><linearGradient id="S2w73RYcJka7XmMmG50Olf" x1="7.08" x2="21.248" y1="7.513" y2="22.582" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#6d7479"/><stop offset="1" stop-color="#323538"/></linearGradient><circle cx="16" cy="17" r="5" fill="url(#S2w73RYcJka7XmMmG50Olf)"/><linearGradient id="S2w73RYcJka7XmMmG50Olg" x1="16.072" x2="19.941" y1="11.611" y2="20.794" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fcfcfc"/><stop offset="1" stop-color="#c3c9cd"/></linearGradient><path fill="url(#S2w73RYcJka7XmMmG50Olg)" d="M16,12c-0.688,0-1.342,0.14-1.939,0.391C14.037,12.593,14,12.791,14,13c0,2.761,2.239,5,5,5	c0.688,0,1.342-0.14,1.939-0.391C20.963,17.407,21,17.209,21,17C21,14.239,18.761,12,16,12z"/><path fill="#881421" d="M30,24c0,0-0.72,5.06-4.01,6.57c-0.02,0.01-0.04,0.02-0.06,0.03c-0.03,0.01-0.06,0.03-0.09,0.04	C25.94,30.44,26,30.22,26,30c0-1.1-1.34-2-3-2c-1.24,0-2.3,0.5-2.75,1.21c-0.11-0.11-0.2-0.22-0.29-0.34C18.41,26.81,18,24,18,24	s2.69,2,6,2S30,24,30,24z"/><path fill="#c94f60" d="M26,30c0,0.22-0.06,0.44-0.16,0.64C25.29,30.86,24.68,31,24,31c-1.55,0-2.72-0.67-3.59-1.6	c-0.06-0.06-0.11-0.12-0.16-0.19C20.7,28.5,21.76,28,23,28C24.66,28,26,28.9,26,30z"/><linearGradient id="S2w73RYcJka7XmMmG50Olh" x1="24" x2="24" y1="15.659" y2="33.13" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#f19a00"/><stop offset="1" stop-color="#eb8100"/></linearGradient><path fill="url(#S2w73RYcJka7XmMmG50Olh)" d="M24,16.005c-11.46,0.28-10.447,10.07-9.402,12.99c0.637,1.77,1.964,3,3.134,3	c3.134,0,3.677-5.08,4.179-8c0.198-1.23,1.16-1.72,2.089-1.84c0.93,0.12,1.891,0.61,2.089,1.84c0.501,2.92,1.045,8,4.179,8	c1.17,0,2.497-1.23,3.134-3C34.447,26.075,35.46,16.285,24,16.005z"/><linearGradient id="S2w73RYcJka7XmMmG50Oli" x1="24" x2="24" y1="19.783" y2="24.1" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#505559"/><stop offset="1" stop-color="#323538"/></linearGradient><ellipse cx="24" cy="22" fill="url(#S2w73RYcJka7XmMmG50Oli)" rx="4" ry="2"/></svg>
+                                        </div>
                                                        {
                                                             User.getUserData()?
                                                                  
@@ -1316,7 +1318,15 @@ class Land extends React.Component<LandProps,any>{
                                            
                                    </div>
                                    <div className='app-land-link-cont-holder cluster-holder-cont'>
-                                   <div className='app-land-lab-main-cont'>Link Activity</div>
+                                   <div className='app-land-lab-main-cont'>
+                                             <svg 
+                                             className='app-land-lab-main-cont-ico'
+                                             width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                             <rect x="1.5" y="1.5" width="34" height="34" rx="11.5" stroke="currentColor" stroke-width="3"/>
+                                             <path d="M7.53693 27.6692C7.35423 28.4772 7.86117 29.2804 8.6692 29.4631C9.47723 29.6458 10.2804 29.1388 10.4631 28.3308L7.53693 27.6692ZM17.8372 20.2856L18.4988 18.9394L17.8372 20.2856ZM24.3488 16.1714L25.7594 16.6816L24.3488 16.1714ZM29.8013 11.2681C30.5016 10.8255 30.7106 9.89908 30.2681 9.19875C29.8255 8.49841 28.8991 8.28941 28.1987 8.73193L29.8013 11.2681ZM10.4631 28.3308C10.8785 26.4934 11.9312 24.3034 13.2911 22.8554C14.6505 21.4079 15.9389 21.024 17.1756 21.6318L18.4988 18.9394C15.5495 17.49 12.8844 18.9063 11.1043 20.8017C9.32461 22.6966 8.05173 25.3923 7.53693 27.6692L10.4631 28.3308ZM17.1756 21.6318C18.7042 22.383 20.045 22.7619 21.2289 22.6833C22.5121 22.5981 23.4451 21.9875 24.0794 21.1478C24.657 20.3833 24.9674 19.4554 25.1824 18.7077C25.4305 17.8452 25.5468 17.2694 25.7594 16.6816L22.9383 15.6611C22.6858 16.3591 22.4684 17.2906 22.2993 17.8785C22.0972 18.5812 21.907 19.0465 21.6857 19.3394C21.5213 19.5571 21.3571 19.6681 21.0301 19.6899C20.6039 19.7182 19.8214 19.5894 18.4988 18.9394L17.1756 21.6318ZM25.7594 16.6816C26.2459 15.3368 26.534 14.5035 27.0524 13.7219C27.5422 12.9834 28.2943 12.2203 29.8013 11.2681L28.1987 8.73193C26.4499 9.83698 25.3415 10.8738 24.5523 12.0637C23.7916 13.2106 23.382 14.4344 22.9383 15.6611L25.7594 16.6816Z" 
+                                             fill="currentColor"/>
+                                             </svg>
+                                        Link Activity</div>
                                    <div className='app-land-visit-card-main-outer-cont'>
                                              <div className='app-land-visit-card-main-cont'>
                                                   <div className='app-land-visit-card-left-cont'>
@@ -1340,7 +1350,17 @@ class Land extends React.Component<LandProps,any>{
                               </div>
 
                               <div className='app-land-link-cont-holder cluster-holder-cont'>
-                              <div className='app-land-lab-main-cont'>Cluster Link</div>
+                              <div className='app-land-lab-main-cont'>                              
+                                   <svg 
+                                   className='app-land-lab-main-cont-ico'
+                                   width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                   <rect x="1.5" y="1.5" width="34" height="34" rx="11.5" stroke="currentColor" stroke-width="3"/>
+                                   <rect x="8" y="10" width="21" height="3" rx="1.5" fill="currentColor"/>
+                                   <rect x="8" y="16" width="21" height="5" rx="2" fill="currentColor"/>
+                                   <rect x="8" y="24" width="21" height="3" rx="1.5" fill="currentColor"/>
+                                   </svg>
+
+                                   Cluster Link</div>
                               <div className='clust-link-main-cont'> 
                                    <div className='clust-link-main-inner-cont'>
                                    <div className='clust-link-main-lock-cont'>
@@ -1391,7 +1411,15 @@ class Land extends React.Component<LandProps,any>{
                               <div className='app-land-link-cont-holder lnk-holder-cust'>
                               
                               
-                              <div className='app-land-lab-main-cont'>Links {this.state.linksData.length}</div>
+                              <div className='app-land-lab-main-cont'>
+                                   <svg 
+                                   className='app-land-lab-main-cont-ico'
+                                   width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                   <rect x="1.5" y="1.5" width="34" height="34" rx="11.5" stroke="currentColor" stroke-width="3"/>
+                                   <path d="M20.5 16.5C19.3 15.3 16 15.0001 15 16.0001L10 21.5C8.5 24 10.4665 27.1611 12.5 27.5C15.5 28 15.5 27 17.5 25.5" stroke="currentColor" stroke-width="3" stroke-linecap="round"/>
+                                   <path d="M16.5 20C17.8057 21.2753 20 22.4999 22 20.9999L24.7049 18.4999L27.4252 15.5149C29.0573 12.8579 26.9176 9.49832 24.7049 9.13815C21.4406 8.60679 21.4406 9.66954 19.2643 11.2637" stroke="currentColor" stroke-width="3" stroke-linecap="round"/>
+                                   </svg>
+                                   Links {this.state.linksData.length}</div>
                               
                               <div className='app-land-serch-main-cont'>
                                         <div className='app-land-top-butt-group-cont'>

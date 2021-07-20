9b11ed604fb11db413c5bf0b8a7e2003de4494e6
cytescale <nikhil@cytescale.com>
2021-07-20 12:10:07 +0530

    server commit

--- pages/src/land/index.tsx
+++ pages/src/land/index.tsx
@@ -517,15 +517,6 @@ const LinkCard:React.FC<any>=(props:any)=>{
                           }
                           
                     </div>
-
-                    <button
-                    className='lnk-lnk-head-edit-butt'
-                    >
-                         <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                         <path d="M2.375 13.8225V16.2292C2.375 16.4508 2.54917 16.625 2.77083 16.625H5.1775C5.28042 16.625 5.38333 16.5854 5.45458 16.5062L14.0996 7.86916L11.1308 4.90041L2.49375 13.5375C2.41458 13.6167 2.375 13.7117 2.375 13.8225ZM16.3954 5.57333C16.7042 5.26458 16.7042 4.76583 16.3954 4.45708L14.5429 2.60458C14.2342 2.29583 13.7354 2.29583 13.4267 2.60458L11.9779 4.05333L14.9467 7.02208L16.3954 5.57333Z" fill="white"/>
-                         </svg>
-                    
-                    </button>
                     <div  className='lnk-lnk-head-right-butt-cont'>
                     <label className="switch">
                          <input type="checkbox" 

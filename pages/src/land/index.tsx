b71b13e470e8ccf00d6e31e6fcf1aad44bb28e3a
cytescale <nikhil@cytescale.com>
2021-07-26 21:50:48 +0530

    server commit

--- pages/src/land/index.tsx
+++ pages/src/land/index.tsx
@@ -791,7 +791,9 @@ class Land extends React.Component<LandProps,any>{
                }}
                >
                     {this.state.makeLinkLoading?
-                    <Spinner animation="border" role="status" variant="light"/>:
+                    <Spinner
+                    className='lnk-lnk-crt-lnk-crt-butt-ico'
+                    animation="border" role="status" variant="light"/>:
                     <svg
                     className='lnk-lnk-crt-lnk-crt-butt-ico'
                     xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9 16.2l-3.5-3.5c-.39-.39-1.01-.39-1.4 0-.39.39-.39 1.01 0 1.4l4.19 4.19c.39.39 1.02.39 1.41 0L20.3 7.7c.39-.39.39-1.01 0-1.4-.39-.39-1.01-.39-1.4 0L9 16.2z"/></svg>

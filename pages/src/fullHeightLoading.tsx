e0fe561432c404b6f099381253b9e76d3258e0f5
cytescale <nikhil@cytescale.com>
2021-07-26 21:04:54 +0530

    server commit

--- pages/src/fullHeightLoading.tsx
+++ pages/src/fullHeightLoading.tsx
@@ -5,7 +5,7 @@ import {Spinner} from 'react-bootstrap';
 const loading:React.FC<any> = (props:any)=>{
      return (
           <div className='app-full-loading-main-cont'>
-                <Spinner animation="border" role="status" variant="light"/>
+                <Spinner animation="border" role="status" variant="dark"/>
                
           </div>
      );

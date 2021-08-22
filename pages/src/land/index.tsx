38ec32ba29f65c41d0b652b73efe3cf757400332
cytescale <nikhil@cytescale.com>
2021-08-22 23:02:58 +0530

    server commit 0.1.5.2

--- pages/src/land/index.tsx
+++ pages/src/land/index.tsx
@@ -1516,7 +1516,7 @@ class Land extends React.Component<LandProps,any>{
                                              
                                         </div>
                                    
-                                        <div className='app-land-link-cont-holder cluster-holder-cont'> 
+                                        {/* <div className='app-land-link-cont-holder cluster-holder-cont'> 
                                         <RenderAccordion
                                         default={false} 
                                         titleBar={(expanded:any)=>{
@@ -1542,17 +1542,9 @@ class Land extends React.Component<LandProps,any>{
                                              )
                                         }}
                                         >
-                                        <div className='app-land-visit-card-main-outer-cont'>
-                                             <div className='app-land-visit-card-main-cont'>
-                                                  <div className='app-land-visit-card-left-cont'>
-                                                       <div className='app-land-visit-card-left-lab'>Clustor Visitors </div>
-                                                       <div className='app-land-visit-card-left-time'>Last 15 days</div>
-                                                  </div>
-                                                  <LandVisitChart darkMode={this.state.darkMode} uid={User?.getUserUid()!} toast={toast}/>
-                                             </div>
-                                             </div>
+                                      
                                         </RenderAccordion>
-                                        </div> 
+                                        </div>  */}
                                    <div className='app-land-link-cont-holder cluster-holder-cont'>
                                    <RenderAccordion
                                         default={false} 
@@ -1568,7 +1560,7 @@ class Land extends React.Component<LandProps,any>{
                                                        <rect x="8" y="24" width="21" height="3" rx="1.5" fill="currentColor"/>
                                                        </svg>
 
-                                                       Cluster Link
+                                                       Cluster
                                                    <div className='app-accord-drop-ico-main-cont'>
                                                         {
                                                              expanded?
@@ -1581,7 +1573,16 @@ class Land extends React.Component<LandProps,any>{
                                              )
                                         }}
                                         >
-                                         <div className='clust-link-main-cont'> 
+                                           <div className='app-land-visit-card-main-outer-cont'>
+                                             <div className='app-land-visit-card-main-cont'>
+                                                  <div className='app-land-visit-card-left-cont'>
+                                                       <div className='app-land-visit-card-left-lab'>Clustor Visitors </div>
+                                                       <div className='app-land-visit-card-left-time'>Last 15 days</div>
+                                                  </div>
+                                                  <LandVisitChart darkMode={this.state.darkMode} uid={User?.getUserUid()!} toast={toast}/>
+                                             </div>
+                                             </div>
+                                         <div className='clust-link-main-cont'>                                          
                                         <div className='clust-link-main-inner-cont'>
                                         <div className='clust-link-main-lock-cont'>
                                              <svg className='clust-link-main-lock-ico' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM9 8V6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9z"/></svg>
